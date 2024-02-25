0c07878786858143902430b9ba9f46b013d7b0f1
vxshugo <vxshugo@gmail.com>
2024-02-25 20:49:38 +0600

    fix

--- src/components/layout/banner/HomeOneBanner.tsx
+++ src/components/layout/banner/HomeOneBanner.tsx
@@ -93,7 +93,7 @@ const HomeOneBanner = () => {
             instagram
           </Link>
           <Link href="https://wa.me/77713758699?text=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82!%20%F0%9F%91%8B%20%D0%9C%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82..." target="_blank">
-            What'sapp
+            WhatsApp
           </Link>
         </div>
         <button
